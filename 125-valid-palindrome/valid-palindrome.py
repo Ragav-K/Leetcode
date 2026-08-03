f2bdfc6d0fdb90b0d669f188de7a3c81563ad183
Ragav-K <iamragav2k7@gmail.com>
2026-08-03 09:56:06 +0530

    Time: 282 ms (5.00%) | Memory: 13 MB (55.71%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        ans = ""
+        fre = {}
+        for ch in s:
+            if ch.isalnum():
+                ans += ch.lower()
+        if ans == ans[::-1]:
+            return True
+        else:
+            return False
+                
