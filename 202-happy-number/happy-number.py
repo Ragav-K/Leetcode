b60ef89dc3e52cf02f25d9c5626f1f224d520c7c
Ragav-K <iamragav2k7@gmail.com>
2026-08-06 18:27:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.06%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            total = 0
+            while n > 0:
+                total += ((n%10)**2)
+                n //= 10
+            n = total
+        return n == 1
