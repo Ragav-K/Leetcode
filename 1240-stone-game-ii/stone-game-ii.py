3a83b32e01e3325b35e4bc847503c1b73446c249
Ragav-K <iamragav2k7@gmail.com>
2026-08-09 10:46:21 +0530

    Time: 160 ms (56.25%) | Memory: 13.7 MB (63.54%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def stoneGameII(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: int
+        """
+        n = len(piles)
+
+        suffix = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
+        dp = {}
+
+        def solve(i, M):
+            if i >= n:
+                return 0
+
+            if 2 * M >= n - i:
+                return suffix[i]
+
+            if (i, M) in dp:
+                return dp[(i, M)]
+
+            best = 0
+
+            for X in range(1, 2 * M + 1):
+                newM = max(M, X)
+
+                opponent = solve(i + X, newM)
+
+                current = suffix[i] - opponent
+
+                best = max(best, current)
+
+            dp[(i, M)] = best
+            return best
+
+        return solve(0, 1)
